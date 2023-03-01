299da858a58cc29395bfa921fdf51708634b4538
Ian Cole <ian@colecoding.com>
2023-03-01 00:48:25 -0800

    Chapter 3

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const { join } = require('path');
+
+module.exports = {
+  content: [
+    join('app', 'components', '**', '*.hbs'),
+    join('app', 'templates', '**', '*.hbs'),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
